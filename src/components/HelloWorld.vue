e6c4b3e5b66737d74149aa06ba5f12f6ad15c68f
lijinghuan <1072090332@qq.com>
2018-08-24 19:15:16 +0800

    new vuex

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2 >{{msg}}</h2>
+    <hr/>
+    <!-- <h3>{{$store.state.count}}</h3> -->
+    <h3>{{count}}</h3>
+    <div>
+        <button @click="$store.commit('add')">+</button>
+        <button @click="$store.commit('reduce')">-</button>
+    </div>
+  </div>
+</template>
+<script>
+  import store from '@/vuex/store'
+  import {mapState} from 'vuex'
+  export default{
+    data(){
+      return{
+        msg:'Hello Vuex',
+        token:'this is token'
+      }
+    },
+    created(){
+      console.log(this.$store)
+    },
+    methods:{
+    },
+    store,
+    computed:mapState(["count"])
+    // computed:{
+    //   // count(){
+    //   //   return this.$store.state.count
+    //   // },
+
+    // }
+  }
+</script>
